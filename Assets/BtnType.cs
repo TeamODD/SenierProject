ba5568db03e02974a59df7acc5a9ab29dd74d1d2
UTSU2 <ejy6125@gmail.com>
2023-08-01 22:23:20 +0900

    new tmp
    change tmp image

--- Assets/BtnType.cs
+++ Assets/BtnType.cs
@@ -13,10 +13,23 @@ public class BtnType : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
     public static bool isSound = true;
     public CanvasGroup mainGroup;
     public CanvasGroup optionGroup;
+<<<<<<< Updated upstream
     public TextMeshProUGUI Soundtext;
     private void Start()
     {
         defaultScale = buttonScale.localScale;
+=======
+    public TextMeshProUGUI button_text;
+    public Color text_defaultcolor;
+    public TMP_FontAsset font;
+    public TMP_FontAsset tmp_font;
+    private void Start()
+    {
+        defaultScale = buttonScale.localScale;
+        button_text = GetComponentInChildren<TextMeshProUGUI>();
+        text_defaultcolor = button_text.color;
+        font = button_text.font;
+>>>>>>> Stashed changes
     }
     
     public void MainOnBtnClick()
@@ -90,12 +103,23 @@ public class BtnType : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
+<<<<<<< Updated upstream
         buttonScale.localScale = defaultScale * 1.2f;
+=======
+        //buttonScale.localScale = defaultScale * 1.2f;
+        button_text.color = new Color32(250, 226, 0, 255);
+        button_text.font = tmp_font;
+>>>>>>> Stashed changes
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         buttonScale.localScale = defaultScale;
+<<<<<<< Updated upstream
+=======
+        button_text.color = text_defaultcolor;
+        button_text.font = font;
+>>>>>>> Stashed changes
     }
 }
 
