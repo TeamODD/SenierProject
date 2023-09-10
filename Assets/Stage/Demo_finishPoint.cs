be347a47367e370441d731ed8248f5c6933296e4
Artiren <sketchhydrogenmagnesium@gmail.com>
2023-09-10 17:48:30 +0900

    일부 코드 최적화

--- Assets/Stage/Demo_finishPoint.cs
+++ Assets/Stage/Demo_finishPoint.cs
@@ -6,18 +6,6 @@ using UnityEngine.UI;
 public class Demo_finishPoint : MonoBehaviour
 {
     [SerializeField] GameObject panel;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
